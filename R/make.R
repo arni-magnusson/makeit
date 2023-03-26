4c6a05d21694d6b61d1ff38d126f8b08c83296bf
Arni Magnusson <thisisarni@gmail.com>
2023-03-26 16:43:08 +1100

    Mention examples directory

--- R/make.R
+++ R/make.R
@@ -52,6 +52,10 @@
 #' make("01_model.R", "data.dat", "results.dat")
 #' make("02_plots.R", "results.dat", c("plots/A.png", "plots/B.png"))
 #' make("03_tables.R", "results.dat", c("tables/A.csv", "tables/B.csv"))
+#'
+#' # Copy these to working directories and explore
+#' system.file("examples/analysis", package="make")
+#' system.file("examples/sequential", package="make")
 #' }
 #'
 #' @export
