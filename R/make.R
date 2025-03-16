c24cfc7cc9803336f2238f9a6d9d3f63d76369db
Arni Magnusson <thisisarni@gmail.com>
2025-03-16 12:46:19 +1100

    Describe general make() call

--- R/make.R
+++ R/make.R
@@ -19,8 +19,13 @@
 #' @param \dots passed to \code{source}.
 #'
 #' @details
-#' A \code{make()} call has the general form: script \emph{x} uses \emph{y} to
-#' produce \emph{z}.
+#' A \code{make()} call has the general form
+#' \preformatted{
+#' make("analysis.R", "input.dat", "output.dat")
+#' }
+#' which can be read aloud as:
+#'
+#' \dQuote{script \emph{x} uses \emph{y} to produce \emph{z}}
 #'
 #' @return \code{TRUE} or \code{FALSE}, indicating whether the script was run.
 #'
